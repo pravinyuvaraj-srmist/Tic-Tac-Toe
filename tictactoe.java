906288cb2c577e4245bac0c01b52b1a4a6c88dd6
Pravin Yuvaraj <py3900@srmist.edu.in>
2026-04-20 08:29:20 +0530

    nothing

--- tictactoe.java
+++ tictactoe.java
@@ -1,19 +1,44 @@
+import java.util.Random;
+
 public class TicTacToe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    // Entry point
     public static void main(String[] args) {
-        char[][] board = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
-        };
-        printBoard(board);
+        tossAndAssignSymbols();
+        displayTossResult();
     }
 
-    public static void printBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(" " + board[i][0] + " | " + board[i][1] + " | " + board[i][2] + " ");
-            if (i < 2) {
-                System.out.println("---|---|---");
-            }
+    // Toss logic and symbol assignment
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+
+        // Random toss: 0 or 1
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
+    }
+
+    // Display result
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human wins the toss and plays first.");
+        } else {
+            System.out.println("Computer wins the toss and plays first.");
+        }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
     }
 }
