2403a7deac4b0efa44dd302a4ab466b6516097ba
Pravin Yuvaraj <py3900@srmist.edu.in>
2026-04-20 08:21:20 +0530

    Display an empty 3×3 Tic-Tac-Toe board on the console.

---
+++ tictactoe.java
@@ -0,0 +1,19 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
+        printBoard(board);
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" " + board[i][0] + " | " + board[i][1] + " | " + board[i][2] + " ");
+            if (i < 2) {
+                System.out.println("---|---|---");
+            }
+        }
+    }
+}
